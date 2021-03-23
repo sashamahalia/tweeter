a1d2813d14ec9df3e8b6f1c55b211bf9cf984f95
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-23 21:34:40 +0000

    Counter counts down from 140 chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-  console.log('hello world');
+  $('#tweet-text').on('input', function(event) {
+    $(this).siblings('.container').children('.counter').html(140 - $(this).val().length);
+    // console.log(140 - $(this).val().length);
+  })
 });
