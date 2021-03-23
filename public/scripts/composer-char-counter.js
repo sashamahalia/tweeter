35b3c3beb94577de3e45773aa5bc1d4f6ad808f6
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-23 21:56:52 +0000

    Implements styling for the counter if it exceeds the character limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,19 @@
+//gets .counter html via dom traversal, then gives it the value of 140 minus the input length
+const count = function(event) {
+  $(this).siblings('.container').children('.counter').html(140 - $(this).val().length);
+}
+
+//adds class of exceeded to the counter if its char count is negative, removes the class if it's 0 or above.
+const exceeds = function() {
+  if ((140 - $(this).val().length) < 0) {
+    $(this).siblings('.container').children('.counter').addClass('exceeded');
+  } else if ((140 - $(this).val().length) >= 0) {
+    $(this).siblings('.container').children('.counter').removeClass('exceeded');
+  }
+}
+
+
 $(document).ready(function() {
-  $('#tweet-text').on('input', function(event) {
-    $(this).siblings('.container').children('.counter').html(140 - $(this).val().length);
-    // console.log(140 - $(this).val().length);
-  })
+  $('#tweet-text').on('input', count);
+  $('#tweet-text').on('input', exceeds);
 });
