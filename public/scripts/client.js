f4ce8d5af3e8c3850b482ceac6623c9f28795944
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-24 04:06:43 +0000

    Creates createTweetElement function - not yet complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,24 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1616278528933
+}
 
+const createTweetElement = function() {
+  const handle = $('.single-tweet header div ~ h5').html($(`<h4>${this.handle}</h4>`));
+  const avatar = $('.single-tweet img').html($(`<img src="${this.avatars}" alt="avatar image">`));
+  return handle, avatar;
+}
+
+// $(document).ready(createTweetElement);
+
+const $tweet = createTweetElement(tweetData);
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
